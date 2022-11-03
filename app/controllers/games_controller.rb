8cfbba731f948fb85496fb0d1f157e63a9575cb4
darrenchay <darrenchay@gmail.com>
2022-11-03 16:03:34 -0400

    recreated game page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,22 +1,23 @@
-class GameController < ApplicationController
+class GamesController < ApplicationController
   skip_before_action :stop_game
   attr_reader :info
 
   def new
     @game = Game.new
-  end
-  
-  def create
     @info = "Welcome to cup throw! Please choose the items you want to load to your cup"
     session[:game_in_progress] = true
+    
     # Setting values
     bag = Bag.find(current_user.bag)
     player_cup = Cup.create(user_id: current_user.id)
     server_cup = Cup.create(user_id: current_user.id)
   
     @game = Game.create(user_id: current_user.id, bag_id: bag.id, player_cup_id: player_cup.id, server_cup_id: server_cup.id)
-    # session[:game] = @game
   end
+  
+  # def create
+  #   # session[:game] = @game
+  # end
 
   def load
     logger.info "loaded cups"
@@ -31,9 +32,9 @@ class GameController < ApplicationController
   def switch
   end
 
-  def block
+  def roll
   end
 
-  def throw
+  def results
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'game/new'
-  get 'game/create'
-  post 'game/load', to: 'game#load'
-  get 'game/switch'
-  get 'game/block'
-  get 'game/throw'
-  resources :users, only: [:new, :create, :index, :show, :edit, :destroy, :update]
+  resources :games, only: [:new] do
+    member do
+      post :switch #After loading, route to switch, where server attempts to switch and block
+      get :roll #After attempting to switch, route to roll, where player will roll
+      get :results #After rolling, route to results, where player sees score, and is asked to play again (route to start game)
+    end
+  end
+  resources :users, except: :index
   resources :sessions, only: [:new, :create, :destroy]
   root 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
